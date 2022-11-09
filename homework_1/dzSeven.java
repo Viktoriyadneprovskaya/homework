273071d24f4aecc83eaeb5be0abe8f2ed44f3e61
Viktoriyadneprovskaya <vlepetiukh@gmail.com>
2022-11-09 22:23:55 +0300

    delete

--- homework_1/dzSeven.java
+++
@@ -1,9 +0,0 @@
-public class dzSeven {
-  
-    public static void main(String[] args) {
-          
-        for (int i=0; i<3; i++) {
-			System.out.println((int) (Math.random() * 100));
-         }
-	}
-}
