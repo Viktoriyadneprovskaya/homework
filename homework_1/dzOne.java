99c55d497799a2b6a42665a7c651e1e50b4c6f71
Viktoriyadneprovskaya <vlepetiukh@gmail.com>
2022-11-09 22:22:09 +0300

    homework_1

---
+++ homework_1/dzOne.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class dzOne {
+  
+    public static void main(String[] args) {
+          
+        Scanner in = new Scanner(System.in);
+		System.out.print("Input a number: ");
+        int num = in.nextInt();
+         
+		System.out.print("Your number: " + num);
+		in.close();
+    }
+}
