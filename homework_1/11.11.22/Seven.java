e5e3bc4a52d4f6041a99f51c80907179e5ebd370
Viktoriyadneprovskaya <vlepetiukh@gmail.com>
2022-11-11 20:17:48 +0300

    added dz 11/11/22

---
+++ homework_1/11.11.22/Seven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Seven {
+
+  public static void main (String[] args) {
+     Scanner in = new Scanner(System.in);
+     String number = new String();
+     System.out.print("Input num: ");
+     String number = in.nextString();
+     System.out.println("Inserted number: " + number);
+
+
+
+}
+}
+
+//НЕ ЗНАЮ КАК СДЕЛАТЬ ЭТО ЗАДАНИЕ
