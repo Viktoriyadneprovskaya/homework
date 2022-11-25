bcd28d5fbe2b6994be17d04999f05778486b8ac7
Viktoriyadneprovskaya <vlepetiukh@gmail.com>
2022-11-25 20:17:53 +0300

    chanches according to requests

--- homework_18_11_22/DzTwo.java
+++ homework_18_11_22/DzTwo.java
@@ -8,16 +8,17 @@ public class DzTwo {
           Scanner input = new Scanner(System.in);
           System.out.println("Enter the massive max num value to show(num must be bigger than 1: ");
           int size = input.nextInt();
-          if (size <= 0) {
-          System.out.println("You entered error number. Number must be bigger than 1.");
-          } 
-          else {
+          if (size <= 1) {
+		  do {
+          System.out.println("You entered error number. Number must be bigger than 1. Enter again:");
+          size = input.nextInt();
+          } while(size <= 1);
+          }
 		  int[] ArrayName = new int[size];
           System.out.println("The numbers between your number and 1:");
           for (int i = size-1; i > 1; i--) {
           System.out.print(i + " ");
          }
-         }   
          }
          }
 		 
